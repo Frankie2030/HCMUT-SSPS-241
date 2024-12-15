082b296630b6ae1671fa702575c3a56a61233b28
Frankie2030 <khoi.nguyenngoccs3008@hcmut.edu.vn>
2024-12-15 17:46:08 +0700

    fix documentation and change port TO 5001

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -28,7 +28,7 @@ import buyingLogRoutes from "./routes/buyingLogRoutes.js";
 
 await connectDB();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 const app = express();
 
